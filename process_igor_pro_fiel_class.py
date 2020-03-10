eac1d2fbe48fc742dbab33c94fadd32e87ab6fe8
kononovdesk <kononovdesk@kononovdesk.e1.physik.tu-dortmund.de>
2020-03-10 14:03:07 +0100

    Add location = (250, 250) to PySimpleGUI window instance, in order to place window not in the corner

--- process_igor_pro_fiel_class.py
+++ process_igor_pro_fiel_class.py
@@ -101,6 +101,7 @@ class Uphos:
     # def plotAll(self):
     #     for i in self.data[1]:
     #         plotData
+    
     def plotData(self, xy_label = (True,True), axExtern = None,  title = None, interactive = True):
         '''
         Plots data of an object into an Image.
@@ -155,8 +156,27 @@ class Uphos:
                 button2pos._button = bcut2
                 button3pos._button = bcut3
         return im
-
+    
+    def sumAllDataFrames(self, data =None):
+        '''
+        Sum all panda.dataFrames element wise
+        '''
+        if data: data = data
+        else: data = self.data
+        cnt = 0
+        for i in data:
+            if cnt == 0:
+                data_frame_sum = i[1]
+            else:
+                data_frame_sum = data_frame_sum.add(i[1], axis='index', fill_value=0.0)
+                #else: data_frame_sum = data_frame_sum.merge(i[1])
+            cnt +=1
+        return data_frame_sum
+    
     def plotOverview(self, data = None):
+        '''
+        Plots DataFrame and Integratet Plots alon x an y axes
+        '''
         if data is not None:
             tmp_data = data
         else:
@@ -166,28 +186,29 @@ class Uphos:
         main_ax = fig.add_subplot(grid[1:4, :-1])       
         x,y = tmp_data.index.values, tmp_data.columns.values
         extent = np.min(x), np.max(x), np.min(y), np.max(y)
-        im = plt.imshow(tmp_data.T, extent = extent, origin = 'lower', cmap='hot',  aspect = 'auto')
-        plt.xlabel(tmp_data.index.name)
-        plt.ylabel(tmp_data.columns.name)
+        im = plt.imshow(tmp_data, extent = extent, origin = 'lower', cmap='hot',  aspect = 'auto')
         main_xlim = plt.xlim()
         main_ylim = plt.ylim()
+        plt.xlabel(tmp_data.index.name)
+        plt.ylabel(tmp_data.columns.name)
         cbar_ax = fig.add_axes([0.01, 0.95, 0.9, 0.05])
         plt.colorbar(im, cax = cbar_ax, orientation='horizontal')     
         y_int = fig.add_subplot(grid[1:4, -1])
         y_int.get_yaxis().set_ticks([])
         #plt.ticklabel_format(axis='x', style='sci',scilimits=(0,0), useMathText = True)
-        plt.ylim(main_ylim)
-        yred = self.reduceY(data = tmp_data)
+        #plt.ylim(main_ylim)
+        yred = self.reduceY(tmp_data)
         plt.xticks(rotation='vertical')
-        plt.plot(yred.values, yred.index.values)
+        y_int.plot(yred.values, yred.index.values,'o')
         #x_int = fig.add_subplot(grid[-1, 1:])
         x_int = fig.add_subplot(grid[0, :-1])
         x_int.set_title(self.workingPath)
         x_int.get_xaxis().set_ticks([])
         #plt.ticklabel_format(axis='y', style='sci', scilimits=(0,0), useMathText = True)
-        plt.xlim(main_xlim)
-        xred = self.reduceX(data = tmp_data)
-        plt.plot(xred)
+        #plt.xlim(main_xlim)
+        xred = self.reduceX(tmp_data)
+        x_int.plot(xred,'o')
+        fig.tight_layout()
         return fig
     
     def reduceX(self, data = None):
@@ -360,6 +381,76 @@ def plotRed(dataSet,info, currentPlot = False):
 
 
 buttoncolor = 'lightskyblue'#'lightgoldenrodyellow'
+
+def savitzky_golay(y, window_size, order, deriv=0, rate=1):
+    r"""Smooth (and optionally differentiate) data with a Savitzky-Golay filter.
+    The Savitzky-Golay filter removes high frequency noise from data.
+    It has the advantage of preserving the original shape and
+    features of the signal better than other types of filtering
+    approaches, such as moving averages techniques.
+    Parameters
+    ----------
+    y : array_like, shape (N,)
+        the values of the time history of the signal.
+    window_size : int
+        the length of the window. Must be an odd integer number.
+    order : int
+        the order of the polynomial used in the filtering.
+        Must be less then `window_size` - 1.
+    deriv: int
+        the order of the derivative to compute (default = 0 means only smoothing)
+    Returns
+    -------
+    ys : ndarray, shape (N)
+        the smoothed signal (or it's n-th derivative).
+    Notes
+    -----
+    The Savitzky-Golay is a type of low-pass filter, particularly
+    suited for smoothing noisy data. The main idea behind this
+    approach is to make for each point a least-square fit with a
+    polynomial of high order over a odd-sized window centered at
+    the point.
+    Examples
+    --------
+    t = np.linspace(-4, 4, 500)
+    y = np.exp( -t**2 ) + np.random.normal(0, 0.05, t.shape)
+    ysg = savitzky_golay(y, window_size=31, order=4)
+    import matplotlib.pyplot as plt
+    plt.plot(t, y, label='Noisy signal')
+    plt.plot(t, np.exp(-t**2), 'k', lw=1.5, label='Original signal')
+    plt.plot(t, ysg, 'r', label='Filtered signal')
+    plt.legend()
+    plt.show()
+    References
+    ----------
+    .. [1] A. Savitzky, M. J. E. Golay, Smoothing and Differentiation of
+       Data by Simplified Least Squares Procedures. Analytical
+       Chemistry, 1964, 36 (8), pp 1627-1639.
+    .. [2] Numerical Recipes 3rd Edition: The Art of Scientific Computing
+       W.H. Press, S.A. Teukolsky, W.T. Vetterling, B.P. Flannery
+       Cambridge University Press ISBN-13: 9780521880688
+    """
+    from math import factorial    
+    try:
+        window_size = np.abs(np.int(window_size))
+        order = np.abs(np.int(order))
+    except ValueError as msg:
+        raise ValueError("window_size and order have to be of type int")
+    if window_size % 2 != 1 or window_size < 1:
+        raise TypeError("window_size size must be a positive odd number")
+    if window_size < order + 2:
+        raise TypeError("window_size is too small for the polynomials order")
+    order_range = range(order+1)
+    half_window = (window_size -1) // 2
+    # precompute coefficients
+    b = np.mat([[k**i for i in order_range] for k in range(-half_window, half_window+1)])
+    m = np.linalg.pinv(b).A[deriv] * rate**deriv * factorial(deriv)
+    # pad the signal at the extremes with
+    # values taken from the signal itself
+    firstvals = y[0] - np.abs( y[1:half_window+1][::-1] - y[0] )
+    lastvals = y[-1] + np.abs(y[-half_window-1:-1][::-1] - y[-1])
+    y = np.concatenate((firstvals, y, lastvals))
+    return np.convolve( m[::-1], y, mode='valid')
 
 def grab_dic(data):
     info_list = []
@@ -593,7 +684,7 @@ def main():
         [sg.Menu(menu_def, )],      
         [sg.Output(size=(60, 20))]      
              ]      
-    window = sg.Window("UPhoS", default_element_size=(15, 1), auto_size_text=False, auto_size_buttons=False, default_button_element_size=(15, 1)).Layout(layout)
+    window = sg.Window("UPhoS", default_element_size=(15, 1), auto_size_text=False, auto_size_buttons=False, location=(250, 250), default_button_element_size=(15, 1)).Layout(layout)
     win = window.Finalize()
     # ------ Loop & Process button menu choices ------ #      
     while True:      
